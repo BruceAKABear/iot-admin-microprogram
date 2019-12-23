9700208543cf26d8949a5e8626feb0b072ac0d59
dengyi <916295934@qq.com>
2019-12-23 20:49:03 +0800

    修改api请求

--- common/vmeitime-http/index.js
+++ common/vmeitime-http/index.js
@@ -1,12 +1,4 @@
 import http from './interface'
-
-/**
- * 将业务所有接口统一起来便于维护
- * 如果项目很大可以将 url 独立成文件，接口分成不同的模块
- * 
- */
-
-// 单独导出(测试接口) import {test} from '@/common/vmeitime-http/'
 export const test = (data) => {
 	/* http.config.baseUrl = "http://localhost:8080/api/"
 	//设置请求前拦截器
